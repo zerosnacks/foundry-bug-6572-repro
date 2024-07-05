1772267f7deff3d3bc8f02350f95068d35e2a974
zerosnacks <zerosnacks@protonmail.com>
2024-07-05 14:51:50 +0200

    inherit deps

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {FOO} from "foundry-bug-6572-repro-lib/Constants.sol";
+import {Bar} from "foundry-bug-6572-repro-lib/Errors.sol";
+
 contract Counter {
     uint256 public number;
 
@@ -10,5 +13,13 @@ contract Counter {
 
     function increment() public {
         number++;
+    }
+
+    function foo() returns (uint256) {
+        return FOO;
+    }
+
+    function barErr() {
+        revert Bar();
     }
 }
